06288bf0434083995d949e379c4c3d95bde70365
ZooBaer404 <zubairstudytech@gmail.com>
2025-07-14 23:06:42 +0430

    feat: open input and output files

--- src/main/java/htmlmd/app/App.java
+++ src/main/java/htmlmd/app/App.java
@@ -2,17 +2,23 @@ package htmlmd.app;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
-import org.jsoup.Jsoup;
+import java.util.Scanner;
+import java.util.List;
+import org.jsoup.parser.Parser;
+import org.jsoup.nodes.Node;
 import java.io.*;
 
 public class App {
     // private static final Logger logger = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
-        File markdownFile;
-        File htmlFile;
+        File inputFile = null;
+        File outputFile = null;
+        Reader inputFileReader = null;
+        Reader outputFileReader = null;
         boolean htmlToMd = true;
 
+        System.out.println("Parsing arguments...");
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("--input")) {
                 if (args.length == i) {
@@ -26,16 +32,14 @@ public class App {
                 // }
 
                 if (args[i+1].endsWith(".md")) {
-                    markdownFile = new File(args[i+1]);
+                    inputFile = new File(args[i+1]);
                     htmlToMd = false;
-                    // System.out.println("input file is " + markdownFile.getName());
+                    System.out.println("input file is " + inputFile.getName());
                 } else if (args[i+1].endsWith(".html")) {
-                    htmlFile = new File(args[i+1]);
+                    inputFile = new File(args[i+1]);
                     htmlToMd = true;
-                    // System.out.println("input file is " + htmlFile.getName());
+                    System.out.println("input file is " + inputFile.getName());
                 }
-
-
             } else if (args[i].equals("--output")) {
                 if (args.length == i) {
                     System.out.println("invalid length of arguments!!!");
@@ -48,29 +52,64 @@ public class App {
                 // }
 
                 if (args[i+1].endsWith(".md")) {
-                    markdownFile = new File(args[i+1]);
+                    htmlToMd = true;
+                    outputFile = new File(args[i+1]);
                     if (!htmlToMd) {
-                        System.out.println("error: can't convert from markdown to markdown");
+                        System.out.println("error: can't convert from markdown to markdown " + args[i+1]);
                         return;
                     }
                     // System.out.println("output file is " + markdownFile.getName());
                 } else if (args[i+1].endsWith(".html")) {
-                    htmlFile = new File(args[i+1]);
+                    htmlToMd = false;
+                    outputFile = new File(args[i+1]);
                     if (htmlToMd) {
-                        System.out.println("error: can't convert from html to html");
+                        System.out.println("error: can't convert from html to html " + args[i+1]);
                         return;
                     }
                     // System.out.println("output file is " + htmlFile.getName());
                 }
-
             }
 
             System.out.print(args[i] + " ");
         }
         System.out.println("");
 
+        if (inputFile == null) {
+            System.out.println("error: input file not specified!!");
+            return;
+        }
+
+        if (outputFile == null) {
+            String inputFileName = inputFile.getName();
+            String autoOutputFile = inputFileName.substring(0, inputFileName.lastIndexOf(".")).concat(((htmlToMd) ? ".md" : ".html"));
+            System.out.println("info: output file is automatically set to " + autoOutputFile);
+        }
+
+        try {
+            inputFileReader = new FileReader(inputFile);
+            outputFileReader = new FileReader(outputFile);
+        } catch (FileNotFoundException ex) {
+            ex.getMessage();
+        }
+
+        System.out.println("Done!");
+        System.out.println("Parsing input file...");
+
+        StringBuilder inputText = new StringBuilder();
+        // HtmlTreeBuilder htmlTreeBuilder = new HtmlTreeBuilder();
+        Parser htmlParser = Parser.htmlParser();
+
+        List<Node> nodeList = htmlParser.parseFragmentInput(inputFileReader, null, "");
+
+        for (Node node : nodeList) {
+            System.out.println(node.nodeName());
+        }
 
 
 
+        System.out.println("Done!");
+        System.out.println("Saving output file...");
+
+        System.out.println("Done!");
     }
 }
